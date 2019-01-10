158d5c6eee5a462b17963b61ffe8a0a0f7c016d9
Kristian Clark <saidar72.com>
2019-01-09 19:14:13 -0600

    Saying hello to myself

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 
-namespace HelloWorld
+public class  Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {   
+        string name = "";
+        int age = 0;
+        int year = 0;
+          
            
-            Console.WriteLine("Hello Kristian!");
-            string yourName = Console.ReadLine();
+        Console.WriteLine("Please enter your name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Please enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the year: ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Hello! My name is {0} and I am {1} years old I was born in {2}.", name, age, year-age);
+	}
             
             
-        }
-    }
 }
+    
+
