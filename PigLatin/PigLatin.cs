c7f19cbcca517f9c5089b0a18f6c51e127a90d64
Kristian Clark <saidar72.com>
2019-01-16 18:28:58 -0600

    PigLatin

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -6,7 +6,14 @@ namespace PigLatin
     {
         public static void Main()
         {
-            // your code goes here
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string firstThird = alphabet.Substring(0, 8);
+            string secondThird = alphabet.Substring(8, 8);
+            string thirdThird = alphabet.Substring(17);
+
+            Console.WriteLine("First Third: " + firstThird);
+            Console.WriteLine("Second Third: " + secondThird);
+            Console.WriteLine("Third Third: " + thirdThird);
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
