ecea1e3080a6b8d0c1c5bb15316aa9a5479f2348
Kristian Clark <saidar72.com>
2019-01-28 23:26:07 -0600

    wrote conditional statement vowel rule

---
+++ Candles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candles = new int[] { 6, 1, 8, 4, 9, 2 };
+            int length = candles.Length;
+            int count = BirthdayCakeCounter(candles.Length, candles);
+            System.Console.WriteLine($"Can blow out {count} candles");
+
+        }
+
+        public static int BirthdayCakeCounter(int length, int[] candles)
+        {
+            int tallest = 0;
+            int count = 0;
+
+            foreach (var candle in candles)
+            {
+                if (candle > tallest)
+                {
+                    tallest = candle;
+                    count = 0;
+                }
+                if (candle == tallest)
+                {
+                    return count++;
+                }
+            }
+            return count;
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
