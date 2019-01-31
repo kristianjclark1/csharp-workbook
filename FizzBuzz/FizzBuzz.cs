5c84572c94648ed910dbe88f20705df32c274504
Kristian Clark <saidar72.com>
2019-01-30 18:01:31 -0600

    added commit

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,31 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 50; i++)
+            {
+                //integers divisible by 3 and 5 will be renamed FizzBuzz//
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz!");
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+
         }
     }
 }
