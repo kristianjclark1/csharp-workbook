2ee2cc34572272d99edd291fd2de503c19856c9b
Kristian Clark <saidar72.com>
2019-02-04 22:11:36 -0600

    Project1 construction

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -9,15 +9,15 @@ namespace Project1
         public static void Main()
         {
             Car snowCar = new Car("white");
-            Car magicalCar = new Car("rainbow");
-            Car hippyCar = new Car("tiedye");
+            Car crashDummy = new Car("yellow");
+            crashDummy.Color = "yellow and white stripes";
+            // Person firstname = new Person("Mary", "Jones");
 
-            Person firstname = new Person("Mary", "Jones");
 
-            Garage smallGarage = new Garage(2);
+            // Garage smallGarage = new Garage(2);
 
-            smallGarage.ParkCar(snowCar, 0);
-            Console.WriteLine(smallGarage.Cars);
+            // smallGarage.ParkCar(snowCar, 0);
+            // Console.WriteLine(smallGarage.Cars);
         }
     }
 
@@ -25,68 +25,94 @@ namespace Project1
     {
         public Car(string initialColor)
         {
-            this.snowCar = initialColor;
+            System.Console.WriteLine("Starting car constructor");
+            System.Console.WriteLine($"This car's color is {this.Color} ");
+            this.Color = initialColor;
+            System.Console.WriteLine($"This car's color is {this.Color} ");
+
+
+            System.Console.WriteLine("Ending car constructor");
+
 
         }
+        private string _color;
+        public string Color
+        {
+            get
+            {
+                //constructing color//
+                System.Console.WriteLine("Starting get color");
+                System.Console.WriteLine("Ending get color");
+                return _color;
+            }
+            set
+            {
+                System.Console.WriteLine("Starting set color");
+                _color = value;
+                System.Console.WriteLine("Ending set color");
+            }
+        }
 
-        public string Color { get; private set; }
+
+
 
 
 
     }
 
-    class Person
+    public class Person
     {
         public Person(string initialfirstName)
         {
             this.firstname = initialfirstName;
         }
-        private string firstname;
+        private string firstname
         {
             get
             {
-                return this.firstname
-    }
-    set
+                return this.firstname + "  ";
+            }
+            set
             {
-
+                string[] names = null;
             }
 
-            
+
         }
     }
 
     class Garage
-{
-    private Car[] cars;
-
-    public Garage(int initialSize)
     {
-        Size = initialSize;
-        this.cars = new Car[initialSize];
-    }
+        private Car[] cars;
 
-    public int Size { get; private set; }
-
-    public void ParkCar(Car car, int spot)
-    {
-        cars[spot] = car;
-    }
-
-    public string Cars
-    {
-        get
+        public Garage(int initialSize)
         {
-            for (int i = 0; i < cars.Length; i++)
+            Size = initialSize;
+            this.cars = new Car[initialSize];
+        }
+
+        public int Size { get; private set; }
+
+        public void ParkCar(Car car, int spot)
+        {
+            cars[spot] = car;
+
+        }
+
+        public string Cars
+        {
+            get
             {
-                if (cars[i] != null)
+                for (int i = 0; i < cars.Length; i++)
                 {
-                    Console.WriteLine(String.Format("The {0} car is in spot {1}.", cars[i].Color, i));
+                    if (cars[i] != null)
+                    {
+                        Console.WriteLine(String.Format("The {0} car is in spot {1}.", cars[i].Color, i));
+                    }
                 }
+                return "That's all!";
             }
-            return "That's all!";
         }
     }
-}
 }
 
