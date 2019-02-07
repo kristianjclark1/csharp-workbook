4cf2fa3a6eb080841b4ed38f3ce1b084a8a49b98
Kristian Clark <saidar72.com>
2019-02-06 21:48:50 -0600

    Mastermind

--- Mastermind/Mastermind.cs
+++ Mastermind/Mastermind.cs
@@ -6,23 +6,25 @@ namespace Mastermind
     {
         // possible letters in code
         public static char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
-        
+
         // size of code
         public static int codeSize = 4;
-        
+
         // number of allowed attempts to crack the code
         public static int allowedAttempts = 10;
-        
+
         // number of tried guesses
         public static int numTry = 0;
-        
+
         // test solution
-        public static char[] solution = new char[] {'a', 'b', 'c', 'd'};
-        
+        public static char[] solution = new char[] { 'a', 'b', 'c', 'd' };
+
+
+
         // game board
         public static string[][] board = new string[allowedAttempts][];
-        
-        
+
+
         public static void Main()
         {
             char[] guess = new char[4];
@@ -35,25 +37,26 @@ namespace Mastermind
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-        
+
         public static bool CheckSolution(char[] guess)
         {
             // Your code here
 
+
             return false;
         }
-        
+
         public static string GenerateHint(char[] guess)
         {
             // Your code here
             return " ";
         }
-        
+
         public static void InsertCode(char[] guess)
         {
             // Your code here
         }
-        
+
         public static void CreateBoard()
         {
             for (var i = 0; i < allowedAttempts; i++)
@@ -65,19 +68,20 @@ namespace Mastermind
                 }
             }
         }
-        
+
         public static void DrawBoard()
         {
             for (var i = 0; i < board.Length; i++)
             {
                 Console.WriteLine("|" + String.Join("|", board[i]));
             }
-            
+
         }
-        
-        public static void GenerateRandomCode() {
+
+        public static void GenerateRandomCode()
+        {
             Random rnd = new Random();
-            for(var i = 0; i < codeSize; i++)
+            for (var i = 0; i < codeSize; i++)
             {
                 solution[i] = letters[rnd.Next(0, letters.Length)];
             }
