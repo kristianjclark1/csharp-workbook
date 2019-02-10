f8c1dd97cf9a64c33cc42e71fa9eb563b33ee5f3
Kristian Clark <saidar72.com>
2019-02-10 15:13:42 -0600

    started rainforest

---
+++ OtherRainForest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OtherRainForest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Company rainForest = new Company("Rainforest");
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Company
+    {
+
+    }
+
+    class Warehouse
+    {
+
+    }
+
+    class Container
+    {
+
+    }
+
+    class Item
+    {
+
+    }
+}
