c57d990c534feadd3fc457bab01e3f6bc0af836a
Kristian Clark <saidar72.com>
2019-02-16 13:41:40 -0600

    Started assignment

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
+
+
 namespace TowersOfHanoi
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string[]> = new Dictionary<string, string>();
+            names.Add("Tower", "A");
+            names.Add("Tower", "B");
+            names.Add("Tower", "C");
+            categories.Add("towers", new string[] { "A", "B", "C" });
+            string towers1 = new towers1("A");
+            string towers2 = new towers2("B");
+            string towers3 = new towers3("C");
+            Stack myStack = new Stack();
             Console.WriteLine("Hello World!");
         }
     }
+    class Block
+    {
+        public int Weight { get; private set; }
+        public Block(int Weight)
+        {
+            this.Weight = Weight;
+        }
+    }
+
+    class Tower
+    {
+        public string Towers { get; set; }
+        public Tower(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    class Game
+    {
+        foreach (KeyValuePair<string, string> kv in names)  
+        {  
+            Console.WriteLine(kv.Key+" "+kv.Value);  
+        }
+
+    public MovePiece(string popOff, string pushOn)
+    {
+
+    }
+}
 }
