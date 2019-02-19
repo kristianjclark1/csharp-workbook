cf03090ad1a4841bab2cb58a94678ec264cc2c1c
Kristian Clark <saidar72.com>
2019-02-18 18:28:38 -0600

    Added info

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace TowersOfHanoi
@@ -8,46 +9,129 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string[]> = new Dictionary<string, string>();
-            names.Add("Tower", "A");
-            names.Add("Tower", "B");
-            names.Add("Tower", "C");
-            categories.Add("towers", new string[] { "A", "B", "C" });
-            string towers1 = new towers1("A");
-            string towers2 = new towers2("B");
-            string towers3 = new towers3("C");
-            Stack myStack = new Stack();
+
+            Game game = new Game();
+            game.PrintBoard();
+            game.AskMove();
+            game.PrintBoard();
             Console.WriteLine("Hello World!");
         }
     }
     class Block
     {
         public int Weight { get; private set; }
-        public Block(int Weight)
+        public Block(int weight)
         {
-            this.Weight = Weight;
+            this.Weight = weight;
         }
+
     }
 
     class Tower
     {
         public string Towers { get; set; }
-        public Tower(string name)
+        public Stack<Block> blocks = new Stack<Block>();
+        public Tower(Stack<Block> block)
         {
-            this.Name = name;
+            this.blocks = block;
         }
+
     }
 
     class Game
     {
-        foreach (KeyValuePair<string, string> kv in names)  
-        {  
-            Console.WriteLine(kv.Key+" "+kv.Value);  
+        Dictionary<string, Tower> towers = new Dictionary<string, Tower>();
+        public Game()
+        {
+            Tower towerA = new Tower(new Stack<Block>());
+            Tower towerB = new Tower(new Stack<Block>());
+            Tower towerC = new Tower(new Stack<Block>());
+            towers["A"] = towerA;
+            towers["B"] = towerB;
+            towers["C"] = towerC;
+            towerA.blocks.Push(new Block(4));
+            towerA.blocks.Push(new Block(3));
+            towerA.blocks.Push(new Block(2));
+            towerA.blocks.Push(new Block(1));
+        }
+        public void PrintBoard()
+        {
+            foreach (KeyValuePair<string, Tower> item in towers)
+            {
+                string blocks = "";
+                foreach (Block block in item.Value.blocks)
+                {
+                    blocks += block.Weight.ToString();
+                }
+                System.Console.WriteLine(item.Key + "|" + blocks);
+
+            }
+
         }
 
-    public MovePiece(string popOff, string pushOn)
-    {
+        public bool IsLegal(Tower from, Tower to)
+        {
+            Block popOff = from.blocks.Peek();
+            Block pushOn = to.blocks.Peek();
+
+            if (pushOn == null)
+            {
+                return true;
+            }
+            else if (popOff.Weight < pushOn.Weight)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public void MovePiece(Tower from, Tower to)
+        { //2.Popoff the piece from the From Tower
+          //3.Push block on to To Tower
+            Block lastBlock = from.blocks.Pop();
+            to.blocks.Push(lastBlock);
+
+
+        }
+
+        public void AskMove()
+        {
+            System.Console.WriteLine("Which tower you do want to move from?");
+            string fromTower = Console.ReadLine();
+            System.Console.WriteLine("Which tower do you want to move to?");
+            string toTower = Console.ReadLine();
+            MovePiece(towers[fromTower], towers[toTower]);
+        }
+
+        public bool CheckForWin()
+        {
+            if (towers["B"].blocks.Count == 4 || towers["C"].blocks.Count == 4)
+            {
+                System.Console.WriteLine("You Won!");
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
 }
-}
+
+
+
