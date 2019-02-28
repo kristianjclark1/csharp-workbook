9c6a34bda12eb31ae3da96180423f5d0dadfd584
Kristian Clark <saidar72.com>
2019-02-28 00:19:19 -0600

    Linq

--- Linq/Linq.cs
+++ Linq/Linq.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Linq
@@ -7,22 +8,39 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
 
-            var source = new int[] { 1, 2, 3, 4 };
-            var reults =
-            from i in source
-            where true
-            select i;
+            students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+            students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+            students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+            students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
 
-            int total = source.Where(x => x % 2 == 0).Select(Square).Sum();
+            //your code here
+            IEnumerable<Student> negativeBalance = from currentStudent in students
+                                                   where currentStudent.Balance < 0
+                                                   select currentStudent;
 
+            System.Console.WriteLine("All the students with a negative balance");
+            foreach (Student currentStudent in negativeBalance)
+            {
+                System.Console.WriteLine(currentStudent.Name);
+            }
 
         }
-        public void int Square(int i)
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int Balance { get; set; }
+
+        public Student(string name, string phone, string address, int balance)
         {
-
+            Name = name;
+            Phone = phone;
+            Address = address;
+            Balance = balance;
         }
-
     }
 }
