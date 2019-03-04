3e180526857f109d8c1719c37c67fbd799614254
Kristian Clark <saidar72.com>
2019-03-04 15:36:28 -0600

    Combined enum with struct

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -6,16 +6,16 @@ namespace Months
     {
         static void Main(string[] args)
         {
-            //Create a program that prints out the day of the week your birthday will fall on for which ever year you type in.
+            foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                var dayEnum = (DayOfWeek)item;
+                System.Console.WriteLine(dayEnum.ToString());
+            }
 
-
-            //Assume the current culture is en-US
-            Console.WriteLine("Please enter the year you would like to know what day your birthday falls on");
-            string bdayYr = Console.ReadLine();
-            DateTime dt = new DateTime(Convert.ToInt32(bdayYr), 9, 10);
-            Console.WriteLine("Your birthday falls on {0} in the year {1}", dt.DayOfWeek, bdayYr);
-
-            Console.WriteLine("Hello World!");
+            var point = new System.Drawing.Point(0, 0);
+            point = new System.Drawing.Point(5, 5);
+            point.X = point.X * point.X;
+            point.Y = point.Y * point.Y;
         }
 
 
@@ -25,9 +25,24 @@ namespace Months
             Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
         }
 
-        enum MonthsOfYearWithNumbers
+
+        public struct structPoint
         {
-            January = 01, February = 02, March = 03, April = 04, May = 05, June = 06, July = 07, August = 08, September = 09, October = 10, November = 11, December = 12
+            public structPoint(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public int X { get; set; }
+            public int Y { get; set; }
         }
+        public struct ClassPoint
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+        }
+
+
     }
 }
