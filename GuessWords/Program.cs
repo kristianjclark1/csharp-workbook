b1d8c9407f18a2ebe74701b10b3904876766875b
Kristian Clark <saidar72.com>
2019-03-07 00:54:36 -0600

    Adding GuessWords

---
+++ GuessWords/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace GuessWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\kristianclark\Documents\words_alpha.txt");
+            Random rng = new Random();
+            int r = rng.Next(lines.Length);
+            //creates random number and selects word for that number from array
+            string compWord = lines[r];
+
+            System.Console.WriteLine("The word I picked is {0}", compWord);
+            string userGuess = Console.ReadLine();
+            int compNum = Array.IndexOf(lines, compWord);
+            int userNum = Array.IndexOf(lines, userGuess);
+            //Console.WriteLine(userNum);
+
+            while (compNum != userNum)
+            {
+                if (compNum < userNum)
+                {
+                    Console.WriteLine("That guess comes after my word! Try again!");
+                }
+                else if (userNum < compNum)
+                {
+                    Console.WriteLine("That guess comes before my word! Try again!");
+                }
+                userGuess = Console.ReadLine();
+                userNum = Array.IndexOf(lines, userGuess);
+            }
+
+            if (compNum == userNum)
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
